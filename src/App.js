11b53440f42e251ae2ddbb42a4e064f4fd21b5c8
AnonymousGrizzly <maks.rogelj.123@gmail.com>
2021-08-09 09:42:42 +0200

    test1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/en/Home';
+import Login from "./components/AuthForm"
+import Profile from ""
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
@@ -14,9 +16,10 @@ export default function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/profile" exact component={Profile}/>
         </Switch>
       </Router>
     </>
-   
   );
 }
